eefc7e533061d3bdfec6554aed11b90bf9e01e47
Austin Zhang <azhang042@gmail.com>
2017-03-06 18:58:07 -0800

    Toggle tchat ratio

--- app/static/js/trusted-ratio-data.js
+++ app/static/js/trusted-ratio-data.js
@@ -28,17 +28,17 @@ function drawChart(response) {
     var columns = [];
     var series = {};
     for (var i = 0; i < data.getNumberOfColumns(); i++) {
-        if (i == 0) {
-            columns.push(i)
-        } else {
-            columns.push({
-                type: data.getColumnType(i),
-                label: data.getColumnLabel(i),
-                calc: function (dataTable, row) {
-                    return null;
-                }
-            });
-        }
+        // if (i == 0) {
+            columns.push(i);
+        // } else {
+        //     columns.push({
+        //         type: data.getColumnType(i),
+        //         label: data.getColumnLabel(i),
+        //         calc: function (dataTable, row) {
+        //             return null;
+        //         }
+        //     });
+        // }
 
         if (i > 0) {
             series[i - 1] = {color: '#CCCCCC'};
