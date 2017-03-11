cefd6b1cf8477e47a195bf9aa002ad3141a66616
Austin Zhang <azhang042@gmail.com>
2017-03-11 11:03:44 -0800

    Add Gear Timer

--- app/static/js/geartimer.js
+++ app/static/js/geartimer.js
@@ -1,15 +1,15 @@
 var gearCount = 0;
 var autonGears = 0;
-var teleOp = 215
+var teleOp = 2*60+15
 var goal = 14;
 var myVar = setInterval(incTimer, 1000);
-var time = 0;
+var time = 2*60+15;
 
 
 
 function incGear(){
     gearCount = gearCount + 1;
-    document.getElementById("timer").innerHTML = gearCount + autonGears;
+//    document.getElementById("timer").innerHTML = gearCount + autonGears;
 
 }
 
@@ -19,14 +19,16 @@ function incAuton(){
 
 function incTimer(){
     console.log("Timer")
-    time = time + 1;
+    time = time - 1;
     var minutes = Math.floor(time / 60);
     var seconds = time - minutes * 60;
+    document.getElementById("timer").innerHTML = String(minutes) + ":" + String(seconds)
+
     var speedGoal = (goal - autonGears) / teleOp;
     var speedCurrent = (gearCount - autonGears) / (seconds)
 
     console.log(speedCurrent)
-    if (speedCurrent < speedGoal){
+    if (speedCurrent > speedGoal){
         document.getElementById("gearProj").innerHTML = "On Target"
     } else {
         document.getElementById("gearProj").innerHTML = "Off Target"
