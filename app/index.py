9878ef2c880708f15135556b572421c2ca07452e
Austin Zhang <azhang042@gmail.com>
2017-03-26 10:47:22 -0700

    Fix

--- app/index.py
+++ app/index.py
@@ -86,7 +86,20 @@ def run_server():
     http_server.serve_forever()
     print("Run2")
     # server.serve_forever()
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
 
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+import sys
+
+sys.stdout = Unbuffered(sys.stdout)
 
 if __name__ == '__main__':
     run_server()
