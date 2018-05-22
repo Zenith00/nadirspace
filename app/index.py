b667ba41e9b50f80498be27e1c0116d195a93f6d
Austin Zhang <zenith00dev@gmail.com>
2018-05-22 16:08:11 -0700

    Update

--- app/index.py
+++ app/index.py
@@ -187,7 +187,7 @@ def logger():
 def run_server():
     app.run(debug=True,
             host='0.0.0.0',
-            port=5000,
+            port=8000,
             threaded=True)
     # print("Running...")
     # # app.wsgi_app = ProxyFix(app.wsgi_app)
