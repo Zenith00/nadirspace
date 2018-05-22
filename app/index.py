ea5a6f035893d24902ce2b046117ccb1cda30e28
Austin Zhang <zenith00dev@gmail.com>
2018-05-22 16:22:27 -0700

    Update

--- app/index.py
+++ app/index.py
@@ -169,9 +169,9 @@ import time
 import subprocess
 import select
 
-f = subprocess.Popen(['tail','-F',LOG_FILE], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-p = select.poll()
-p.register(f.stdout)
+# f = subprocess.Popen(['tail','-F',LOG_FILE], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+# p = select.poll()
+# p.register(f.stdout)
 
 
 @app.route('/logstream')
