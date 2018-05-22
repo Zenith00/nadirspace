cc8e14051e11e67f8545fb6dbd1c3458dc17d69e
Austin Zhang <zenith00dev@gmail.com>
2018-05-22 16:43:51 -0700

    Update

--- app/index.py
+++ app/index.py
@@ -176,29 +176,27 @@ import select
 
 @app.route('/logstream')
 def logger():
-    try:
-        f = subprocess.Popen(['tail', '-F', LOG_FILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        p = select.poll()
-        p.register(f.stdout)
-    except:
-        return LOG_FILE + "\n" + traceback.format_exc()
+
     def logStream():
         # import sh
         # tail = sh.tail("-f", LOG_FILE, _iter=True)
-        line = ""
-        with open(LOG_FILE, "rt") as follow:
-            follow.seek(-64, 2)
-            for block in iter(lambda: follow.read(1024), None):
-                if '\n' in block:
-                    # Only enter this block if we have at least one line to yield.
-                    # The +[''] part is to catch the corner case of when a block
-                    # ends in a newline, in which case it would repeat a line.
-                    for line in (line + block).splitlines(True) + ['']:
-                        if line.endswith('\n'):
-                            yield line
-                    # When exiting the for loop, 'line' has any remaninig text.
-                elif not block:
-                    time.sleep(0.2)
+        try:
+            line = ""
+            with open(LOG_FILE, "rt") as follow:
+                follow.seek(-64, 2)
+                for block in iter(lambda: follow.read(1024), None):
+                    if '\n' in block:
+                        # Only enter this block if we have at least one line to yield.
+                        # The +[''] part is to catch the corner case of when a block
+                        # ends in a newline, in which case it would repeat a line.
+                        for line in (line + block).splitlines(True) + ['']:
+                            if line.endswith('\n'):
+                                yield line
+                        # When exiting the for loop, 'line' has any remaninig text.
+                    elif not block:
+                        time.sleep(0.2)
+        except:
+            yield traceback.format_exc()
         #     # Wait for data.
         #
         # for line in Pygtail(LOG_FILE):
