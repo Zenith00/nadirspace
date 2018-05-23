a8c9ceb137097912087411019d2d973b422416d1
Austin Zhang <zenith00dev@gmail.com>
2018-05-22 17:07:08 -0700

    Update

--- app/index.py
+++ app/index.py
@@ -175,43 +175,36 @@ log_obj = open(LOG_FILE, "r")
 
 import sh
 
+def tail_F(some_file):
+    first_call = True
+    while True:
+        try:
+            with open(some_file) as input:
+                if first_call:
+                    input.seek(0, 2)
+                    first_call = False
+                latest_data = input.read()
+                while True:
+                    if '\n' not in latest_data:
+                        latest_data += input.read()
+                        if '\n' not in latest_data:
+                            yield ''
+                            if not os.path.isfile(some_file):
+                                break
+                            continue
+                    latest_lines = latest_data.split('\n')
+                    if latest_data[-1] != '\n':
+                        latest_data = latest_lines[-1]
+                    else:
+                        latest_data = input.read()
+                    for line in latest_lines[:-1]:
+                        yield line + '\n'
+        except IOError:
+            yield ''
+
 @app.route('/logstream')
 def logger():
-    # log_obj.seek(0, 2)
-    def logStream():
-        try:
-            tail = sh.tail("-F", LOG_FILE, _iter=True)
-            while True:
-                try:
-                    next_line = tail.next()
-                    # print(next_line)
-                    yield "data: {}\n\n".format(next_line)
-                except:
-                    print("Nothing Found")
-        except:
-            yield traceback.format_exc()
-    #
-    # def logStream():
-    #     try:
-    #         while True:
-    #             line = log_obj.readline()
-    #             if not line:
-    #                 time.sleep(0.1)
-    #                 continue
-    #             yield line
-    #     except:
-    #         yield traceback.format_exc()
-    # #     # Wait for data.
-    #
-    # for line in Pygtail(LOG_FILE):
-    #     yield line
-    #     # try:
-    #     #     yield "data: {}\n\n".format(f.stdout.readline())
-    #     #     time.sleep(0.5)
-    #     # except:
-    #     #     print("Nothing Found")
-
-    return Response(logStream(), mimetype="text/event-stream")
+    return Response(tail_F(LOG_FILE), mimetype="text/event-stream")
 
 
 # @run_with_reloader
