68a32d89ba641c872b0a044ec10ef9b6ec5c3c29
Austin Zhang <zenith00dev@gmail.com>
2018-05-22 19:17:25 -0700

    Update

--- app/index.py
+++ app/index.py
@@ -180,26 +180,25 @@ def tail_F(some_file):
     try:
         while True:
             try:
-                with open(some_file) as input:
-                    if first_call:
-                        input.seek(0, 2)
-                        first_call = False
-                    latest_data = input.read()
-                    while True:
+                if first_call:
+                    log_obj.seek(0, 2)
+                    first_call = False
+                latest_data = log_obj.read()
+                while True:
+                    if '\n' not in latest_data:
+                        latest_data += log_obj.read()
                         if '\n' not in latest_data:
-                            latest_data += input.read()
-                            if '\n' not in latest_data:
-                                yield ''
-                                if not os.path.isfile(some_file):
-                                    break
-                                continue
-                        latest_lines = latest_data.split('\n')
-                        if latest_data[-1] != '\n':
-                            latest_data = latest_lines[-1]
-                        else:
-                            latest_data = input.read()
-                        for line in latest_lines[:-1]:
-                            yield line + '\n'
+                            yield ''
+                            if not os.path.isfile(some_file):
+                                break
+                            continue
+                    latest_lines = latest_data.split('\n')
+                    if latest_data[-1] != '\n':
+                        latest_data = latest_lines[-1]
+                    else:
+                        latest_data = log_obj.read()
+                    for line in latest_lines[:-1]:
+                        yield line + '\n'
             except IOError:
                 yield ''
             except:
