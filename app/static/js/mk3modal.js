056c14c774bd47811a856edde014a06aa1db283d
Austin Zhang <zenith00dev@gmail.com>
2018-06-01 20:41:16 -0700

    Update

--- app/static/js/mk3modal.js
+++ app/static/js/mk3modal.js
@@ -49,38 +49,38 @@ $(document).ready(function(){
     $('.modal-wrap, #modal .button').removeClass('visible');
   });
 
-  // var carousel = $('#carousel'),
-  //     slideWidth = 700,
-  //     threshold = slideWidth/3,
-  //     dragStart,
-  //     dragEnd;
+  var carousel = $('#carousel'),
+      slideWidth = 700,
+      threshold = slideWidth/3,
+      dragStart,
+      dragEnd;
 
   setDimensions();
 
-  // $('#next').click(function(){ shiftSlide(-1) })
-  // $('#prev').click(function(){ shiftSlide(1) })
-  //
-  // carousel.on('mousedown', function(){
-  //   if (carousel.hasClass('transition')) return;
-  //   dragStart = event.pageX;
-  //   $(this).on('mousemove', function(){
-  //     dragEnd = event.pageX;
-  //     $(this).css('transform','translateX('+ dragPos() +'px)');
-  //   });
-  //   $(document).on('mouseup', function(){
-  //     if (dragPos() > threshold) { return shiftSlide(1) }
-  //     if (dragPos() < -threshold) { return shiftSlide(-1) }
-  //     shiftSlide(0);
-  //   });
-  // });
+  $('#next').click(function(){ shiftSlide(-1) })
+  $('#prev').click(function(){ shiftSlide(1) })
+
+  carousel.on('mousedown', function(){
+    if (carousel.hasClass('transition')) return;
+    dragStart = event.pageX;
+    $(this).on('mousemove', function(){
+      dragEnd = event.pageX;
+      $(this).css('transform','translateX('+ dragPos() +'px)');
+    });
+    $(document).on('mouseup', function(){
+      if (dragPos() > threshold) { return shiftSlide(1) }
+      if (dragPos() < -threshold) { return shiftSlide(-1) }
+      shiftSlide(0);
+    });
+  });
 
   function setDimensions() {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
      slideWidth = $(window).innerWidth();
     }
-    // $('.carousel-wrap, .slide').css('width', slideWidth);
+    $('.carousel-wrap, .slide').css('width', slideWidth);
     $('.modal').css('max-width', slideWidth);
-    // $('#carousel').css('left', slideWidth * -1)
+    $('#carousel').css('left', slideWidth * -1)
   }
 
   function dragPos() {
@@ -88,40 +88,40 @@ $(document).ready(function(){
   }
 
   function shiftSlide(direction) {
-    // if (carousel.hasClass('transition')) return;
+    if (carousel.hasClass('transition')) return;
     dragEnd = dragStart;
     $(document).off('mouseup')
-    // carousel.off('mousemove')
-    //         .addClass('transition')
-    //         .css('transform','translateX(' + (direction * slideWidth) + 'px)');
-    setTimeout(function(){
-      if (direction === 1) {
-        $('.slide:first').before($('.slide:last'));
-      } else if (direction === -1) {
-        $('.slide:last').after($('.slide:first'));
-      }
-      // carousel.removeClass('transition')
-      // carousel.css('transform','translateX(0px)');
+    carousel.off('mousemove')
+            .addClass('transition')
+            .css('transform','translateX(' + (direction * slideWidth) + 'px)');
+    // setTimeout(function(){
+    //   if (direction === 1) {
+    //     $('.slide:first').before($('.slide:last'));
+    //   } else if (direction === -1) {
+    //     $('.slide:last').after($('.slide:first'));
+    //   }
+      carousel.removeClass('transition')
+      carousel.css('transform','translateX(0px)');
     },700)
   }
 
   function fillModal(id) {
     $('#modal .title').text(modalText[id].title);
     $('#modal .detail').text(modalText[id].detail);
-    // $('#modal .tag').text(modalText[id].tag);
-    // if (modalText[id].link) $('#modal .button').addClass('visible')
-    //                                            .parent()
-    //                                            .attr('href', modalText[id].link)
-    //
-    // $.each($('#modal li'), function(index, value ) {
-    //   $(this).text(modalText[id].bullets[index]);
-    // });
+    $('#modal .tag').text(modalText[id].tag);
+    if (modalText[id].link) $('#modal .button').addClass('visible')
+                                               .parent()
+                                               .attr('href', modalText[id].link)
+
+    $.each($('#modal li'), function(index, value ) {
+      $(this).text(modalText[id].bullets[index]);
+    });
     // $.each($('#modal .slide'), function(index, value) {
     //   $(this).css({
     //     background: "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
     //     backgroundSize: 'cover'
     //   });
-    //
-    // });
+
+    });
   }
 })
